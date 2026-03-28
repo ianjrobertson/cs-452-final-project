872df8dd9813c882948a65081cbed8d29309e823
Ian <ijr011235@gmail.com>
2026-03-27 23:41:26 -0600

    create auth migration

--- oracle/mix.exs
+++ oracle/mix.exs
@@ -40,6 +40,7 @@ defmodule Oracle.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.8.1"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.13"},
@@ -65,7 +66,8 @@ defmodule Oracle.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:pgvector, "~> 0.3"}
     ]
   end
 
---
+++ oracle/priv/repo/migrations/20260328054105_create_markets.exs
@@ -0,0 +1,7 @@
+defmodule Oracle.Repo.Migrations.CreateMarkets do
+  use Ecto.Migration
+
+  def change do
+
+  end
+end
