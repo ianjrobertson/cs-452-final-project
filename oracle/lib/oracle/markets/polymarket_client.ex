14e015731d0924e07f375f78843d744cd7a854ae
Ian <ijr011235@gmail.com>
2026-04-01 21:52:27 -0600

    polymarket client

--- oracle/lib/oracle/markets/polymarket_client.ex
+++ oracle/lib/oracle/markets/polymarket_client.ex
@@ -1,16 +1,44 @@
 defmodule Oracle.Markets.PolymarketClient do
+  @api_url "https://gamma-api.polymarket.com/markets"
   def list_active_markets(opts \\ []) do
-    ## hit the polymarket gamma api to get a list of markets
-    ## return a list of markets. This is what we list for the user to select
+    case Oracle.HTTP.get("#{@api_url}", params: %{active: true, limit: 50}) do
+      {:ok, body} ->
+        {:ok, Enum.map(body, &normalize/1)}
+      {:error, reason} ->
+        {:error, reason}
+    end
   end
 
   def get_market(condition_id) do
-    ## given a specific market, get more information
-    ## This is the data we store in the Market database record.
+    case Oracle.HTTP.get("#{@api_url}/#{condition_id}") do
+      {:ok, body} ->
+        {:ok, normalize(body)}
+      {:error, reason} ->
+        {:error, reason}
+    end
   end
 
+  # See the polymarket API docs for what gets returned. We might want description in the future as well. 
   defp normalize(market) do
-    # Turn the raw json into a struct
+    %{
+      condition_id: market["conditionId"],
+      question: market["question"],
+      active: market["active"],
+      probability: parse_probability(market["outcomePrices"]),
+      end_date: parse_end_date(market["endDateIso"])
+    }
+  end
+
+  defp parse_probability(nil), do: nil
+  defp parse_probability(outcome_prices) do
+    [yes_price | _rest] = Jason.decode!(outcome_prices)
+    String.to_float(yes_price)
+  end
+
+  defp parse_end_date(nil), do: nil
+  defp parse_end_date(end_date) do
+    DateTime.from_iso8601!(end_date)
+    |> DateTime.truncate(:second)
   end
 
 end
