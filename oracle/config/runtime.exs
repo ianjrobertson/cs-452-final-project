dbc75993953bebb672e3c93f0438c2b1e66bd426
Ian <ijr011235@gmail.com>
2026-04-07 23:11:30 -0600

    add dev api key

--- oracle/config/runtime.exs
+++ oracle/config/runtime.exs
@@ -20,6 +20,8 @@ if System.get_env("PHX_SERVER") do
   config :oracle, OracleWeb.Endpoint, server: true
 end
 
+config :oracle, openai_api_key: System.get_env("OPENAI_API_KEY")
+
 if config_env() == :prod do
   database_url =
     System.get_env("DATABASE_URL") ||
